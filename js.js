026f7c21e0e45da3af778028f2683d8886c5f6b2
Zachary <zazzer@live.dk>
2019-05-22 14:14:17 +0200

    Process knap javascript
    Menu knapperne lyser op når du er på en given side

--- js.js
+++ js.js
@@ -32,3 +32,36 @@ setInterval(setTimeout, function() {
     s1.style.display = showSlide1 ? 'block' : 'none';
     s2.style.display = showSlide1 ? 'none' : 'block';
 }, 15000)
+
+
+/* Process knapper */
+
+window.onscroll = function() {forside(), koncept(), appen(), kontakt()};
+
+function forside() {
+    if (document.body.scrollTop > -1 || document.documentElement.scrollTop > -1) {
+        document.getElementById("dot1").className = "dotfarve1";
+    } else { document.getElementById("dot1").className = "dot";
+    }
+}
+
+function koncept() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("dot2").className = "dotfarve2";
+    } else { document.getElementById("dot2").className = "dot";
+    }
+}
+
+function appen() {
+    if (document.body.scrollTop > 1050 || document.documentElement.scrollTop > 1050) {
+        document.getElementById("dot3").className = "dotfarve3";
+    } else { document.getElementById("dot3").className = "dot";
+    }
+}
+
+function kontakt() {
+    if (document.body.scrollTop > 1300 || document.documentElement.scrollTop > 1300) {
+        document.getElementById("dot4").className = "dotfarve4";
+    } else { document.getElementById("dot4").className = "dot";
+    }
+}
